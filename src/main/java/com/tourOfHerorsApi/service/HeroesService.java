936a67f54d636a44d298862e0ce54b95f1aa8f67
SergioRuyDev <sergioruyenator@gmail.com>
2021-04-20 16:05:49 +0400

    Service in progress.

--- src/main/java/com/tourOfHerorsApi/service/HeroesService.java
+++ src/main/java/com/tourOfHerorsApi/service/HeroesService.java
@@ -3,20 +3,29 @@ package com.tourOfHerorsApi.service;
 import com.tourOfHerorsApi.model.Heroes;
 import com.tourOfHerorsApi.repository.HeroesRepo;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.UUID;
 
 @Service
+@Transactional
 @AllArgsConstructor
 public class HeroesService {
 
-    public HeroesRepo heroesRepo;
+    private final HeroesRepo heroesRepo;
 
-    public List<Heroes> getHeroes() {
+    public Heroes addHero(Heroes heroes) {
+        heroes.setName(UUID.randomUUID().toString());
+        return heroesRepo.save(heroes);
+    }
+
+    public List<Heroes> findAllHeroes() {
         return heroesRepo.findAll();
     }
 
-    public  Heroes getById
+    public  Heroes findHeroById(Long id) {
+
+    }
 }
